2b4d48628a9127064d30e2b1e776ce02435d53bb
Coursera Learner <nobody@nowhere.nul>
2020-08-10 07:07:19 +0000

    Did c2prj1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,119 @@
 
 
 void assert_card_valid(card_t c) {
-
+  int flag=0;
+  if((c.value>=2 && c.value<=VALUE_ACE) && (c.suit>=SPADES && c.suit<=CLUBS))
+    flag=1;
+  assert(flag);
 
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  switch(r){
+    case 0:return "STRAIGHT FLUSH";break;
+    case 1:return "FOUR OF A KIND";break;
+    case 2:return "FULL HOUSE";break;
+    case 3:return "FLUSH";break;
+    case 4:return "STRAIGHT";break;
+    case 5:return "THREE OF A KIND";break;
+    case 6:return "TWO PAIR";break;
+    case 7:return "PAIR";break;
+    case 8:return "NOTHING";break;
+    default:printf("Invalid input");exit(0);
+  }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  char v=' ';
+  if (c.value>=2 && c.value<=9){
+    v=48+c.value;
+  }
+  switch(c.value){
+    case 10:v= '0';break;
+    case 11:v= 'J';break;
+    case 12:v= 'Q';break;
+    case 13:v= 'K';break;
+    case 14:v= 'A';
+  }
+  if (c.value>14){
+    printf("invalid input");
+    exit(0);
+  }
+  return v;
 }
 
-
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch(c.suit){
+    case SPADES:return 's';
+    case HEARTS:return 'h';
+    case DIAMONDS:return 'd';
+    case CLUBS:return 'c';
+    default:printf("Invalid input");exit(0); 
+  }
 }
 
 void print_card(card_t c) {
-
+  char v=value_letter(c);
+  char s=suit_letter(c);
+  printf("%c%c",v,s);
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  switch(value_let){
+    case '2':temp.value=2;break;
+    case '3':temp.value=3;break;
+    case '4':temp.value=4;break;
+    case '5':temp.value=5;break;
+    case '6':temp.value=6;break;
+    case '7':temp.value=7;break;
+    case '8':temp.value=8;break;
+    case '9':temp.value=9;break;
+    case '0':temp.value=10;break;
+    case 'A':temp.value=14;break;
+    case 'K':temp.value=13;break;
+    case 'Q':temp.value=12;break;
+    case 'J':temp.value=11;break;
+    default:printf("Invalid input");exit(0);
+  }
+  switch(suit_let){
+    case 's':temp.suit=SPADES;break;
+    case 'h':temp.suit=HEARTS;break;
+    case 'd':temp.suit=DIAMONDS;break;
+    case 'c':temp.suit=CLUBS;break;
+    default:printf("Invalid input");exit(0);
+  }
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
-  card_t temp;
+  card_t temp;char a,b;
+  if(c>51){
+    printf("invalid input");
+    exit(0);
+  }
+  
+  if(c>=0 && c<=12)
+    a='s';
+  else if(c>=13 && c<=25)
+    a='h';
+  else if(c>=26 && c<=38)
+    a='d';
+  else if(c>=39 && c<=51)
+    a='c';
+  
+  if (c%13==0)
+    b='0';
+  else if(c%13==1)
+    b='A';
+  else if(c%13>=2 && c%13 <=9)
+    b=48+(c%13);
+  else if (c%13==10)
+    b='J';
+  else if(c%13==11)
+    b='Q';
+  else if (c%13==12)
+    b='K';
+  temp=card_from_letters(b,a);
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,38 @@
 #include "cards.h"
+#include<stdio.h>
 
 int main(void) {
+  card_t c;int  a;
+  printf("\nEnter card value and suit");
+  scanf("%u",&c.value);
+  scanf("%d",&a);
+  c.suit=a;
+  assert_card_valid(c);
+  print_card(c);
+  char v=value_letter(c);
+  printf("\n%c",v);
+  char s=suit_letter(c);
+  printf("%c",s);
+  int r;
+  printf("\nEnter ranking");
+  scanf("%d",&r);
+  hand_ranking_t t;
+  t=r;
+  printf("\n%s",ranking_to_string(t));
+  card_t c2;
+  printf("\nEnter char for value and suit");
+  char a1,b;
+  scanf(" %c %c",&a1,&b);
+  
+  c2=card_from_letters(a1,b);
+  print_card(c2);
 
+  for (int i=0;i<52;i++){
+    c2=card_from_num(i);
+    print_card(c2);
+    printf(" ");
+  }
+
+  return 0;
 }
+
