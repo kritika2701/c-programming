4abe8b6a2440c41a7e8742dfe0ba187a18f945bf
Coursera Learner <nobody@nowhere.nul>
2021-05-09 13:46:52 +0000

    did assignment

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -1,64 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
-void readM(char m[10][10],FILE *f){
-  int c,i=0,j=0;
-  while((c=fgetc(f))!=EOF){
-    if(c=='\n'){
-      if(j!=10){
-	fprintf(stderr, "invalid number of row elements\n");
-	exit(EXIT_FAILURE);
+
+int verify(FILE *f){
+  int c=0;int col=0;
+  c=fgetc(f);
+  while(!(c==EOF)){
+    for(int i=0;i<10;i++){
+      if(c=='\n'){
+	fprintf(stderr, "char error\n");
+	return 0;
       }
-      i++;j=0;
-      continue;
+      c=fgetc(f);
     }
-    if(i>=10){
-      fprintf(stderr, "extra row\n");
-      exit(EXIT_FAILURE);
+    if(c!='\n'){
+      fprintf(stderr, "row err\n");
+      return 0;
     }
-    m[i][j]=c;
-    j++;
+    c=fgetc(f);
+    col++;
   }
-  if(i<10){
-    fprintf(stderr, "less rows\n");
-    exit(EXIT_FAILURE);
+  if(col==10)
+    return 1;
+  else{
+    fprintf(stderr, "column err\n");
+    return 0;
   }
 }
 
 void rotate(char m[10][10]){
-  int s=10; int l,r;
-  char tmp;
-  for(int i=0;i<s;i++){
-    l=0;
-    r=s-1;
-    while(l<r){
-      tmp=m[i][l];
-      m[i][l]=m[i][r];
-      m[i][r]=tmp;
-      l++;r--;
-    }
-  }
-  int x,y;
-  for(int i=0;i<s-1;i++){
-    for(int j=0;j<s-1;j++){
-      x=s-1-j;
-      y=s-1-i;
-      tmp=m[i][j];
-      m[i][j]=m[x][y];
-      m[x][y]=tmp;
-    }
-  }
-}
-void print(char m[10][10]){
   for(int i=0;i<10;i++){
     for(int j=0;j<10;j++){
-      printf("%c",m[i][j]);
+      char temp=m[i][j];
+      m[i][j]=m[j][i];
+      m[j][i]=temp;
     }
-    printf("\n");
   }
+  for(int i=0;i<10;i++){
+    for(int j=0;j<5;j++){
+      char temp=m[i][j];
+      m[i][j]=m[i][9-j];
+      m[i][9-j]=temp;
+    }
+  }
+  return ;
 }
+
 int main(int argc, char ** argv){
   if(argc!=2){
-    fprintf(stderr, "invalid number of inputs\n");
+    fprintf(stderr,"invalid number of inputs\n");
     return EXIT_FAILURE;
   }
   FILE * f=fopen(argv[1],"r");
@@ -66,13 +55,27 @@ int main(int argc, char ** argv){
     fprintf(stderr,"could not open file\n");
     return EXIT_FAILURE;
   }
-  
+  if(!verify(f))
+    return EXIT_FAILURE;
   char m[10][10];
-  readM(m,f);
+  int c=0;
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      c=fgetc(f);
+      if(c=='\n')
+	c=fgetc(f);
+      m[i][j]=c;
+    }
+  }
   rotate(m);
-  print(m);
-  if((fclose(f))!=0){
-    fprintf(stderr, "could not close file\n");
+  for(int x=0;x<10;x++){
+    for(int y=0;y<10;y++){
+      printf("%c",m[x][y]);
+    }
+    printf("\n");
+  }
+  if(fclose(f)!=0){
+    fprintf(stderr,"failed to close the input file\n");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
