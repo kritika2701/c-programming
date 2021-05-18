17dae857fee21ab2eac2e141feefdeb6bc0271b3
Coursera Learner <nobody@nowhere.nul>
2021-05-18 16:28:10 +0000

    did assignment

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -1,20 +1,19 @@
-#include<ctype.h>
 #include<stdio.h>
 #include<stdlib.h>
 
 int checkInp(FILE *f){
-  int c=0, col=0;
+  int c=0;int col=0;
   c=fgetc(f);
   while(!(c==EOF)){
     for(int i=0;i<10;i++){
       if(c=='\n'){
-	fprintf(stderr, "character error");
+	fprintf(stderr, "character error\n");
 	return 0;
       }
       c=fgetc(f);
     }
     if(c!='\n'){
-      fprintf(stderr, "wrong rows");
+      fprintf(stderr, "wrong rows\n");
       return 0;
     }
     c=fgetc(f);
@@ -23,60 +22,56 @@ int checkInp(FILE *f){
   if(col==10)
     return 1;
   else{
-    fprintf(stderr, "wrong columns");
+    fprintf(stderr, "wrong columns\n");
     return 0;
   }
 }
 
-void rotate(char matrix[10][10]){
+void rotate(FILE *f){
+  int c;char matrix[10][10];
+  while((c=fgetc(f))!=EOF){
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+	matrix[i][j]=c;
+      }
+    }
+  }
+  char new[10][10];
   for(int i=0;i<10;i++){
     for(int j=0;j<10;j++){
-      char temp=matrix[i][j];
-      matrix[i][j]=matrix[j][i];
-      matrix[j][i]=temp;
+      new[i][j]=matrix[j][i];
     }
   }
   for(int i=0;i<10;i++){
-    for(int j=0;j<5;j++){
-      char temp=matrix[i][j];
-      matrix[i][j]=matrix[i][9-j];
-      matrix[i][9-j]=temp;
+    int k=0;
+    for(int j=9;j>=0;j--){
+      matrix[i][k]=new[i][j];
+      k++;
     }
   }
-  return;
-}
-
-int main(int argc, char ** argv){
-  if(argc!=2){
-    fprintf(stderr, "wrong number of arguments");
-    return EXIT_FAILURE;
-  }
-  FILE * f= fopen(argv[1],"r");
-  if(f==NULL){
-    perror("could not open file");
-    return EXIT_FAILURE;
-  }
-  if(!(checkInp(f)))
-    return EXIT_FAILURE;
-  char matrix[10][10];
-  int c=0;
-  for(int i=0;i<10;i++){
-    for(int j=0;j<10;j++){
-      c=fgetc(f);
-      if(c=='\n')
-	c=fgetc(f);
-      matrix[i][j]=c;
-    }
-  }
-  rotate(matrix);
   for(int i=0;i<10;i++){
     for(int j=0;j<10;j++){
       printf("%c", matrix[i][j]);
     }
     printf("\n");
   }
+}
+  
+int main(int argc, char ** argv){
+  if(argc!=2){
+    fprintf(stderr, "wrong number of arguments\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f= fopen(argv[1],"r");
+  if(f==NULL){
+    perror("could not open file\n");
+    return EXIT_FAILURE;
+  }
+  if(!(checkInp(f)))
+    return EXIT_FAILURE;
+  rotate(f);
   if(fclose(f)!=0){
-    perror("could not close the file");
+    perror("could not close the file\n");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
