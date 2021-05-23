27665549015db63bccc2945c98bc0e700f0043f6
Coursera Learner <nobody@nowhere.nul>
2021-05-23 06:39:16 +0000

    did assignment

--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -1,13 +1,16 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
 struct _one_count_t {
-  //DEFINE ME
+  char * str;
+  size_t cnt;
 
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
-  //DEFINE ME
+  one_count_t** cntArr;
+  size_t cntSize;
+  size_t unknown;
 };
 typedef struct _counts_t counts_t;
 
